6bd1a8769ce51681d8c575428e3cdea2aa86b51d
thomaswang0822 <thomaswang0822@outlook.com>
2023-04-20 00:50:37 -0700

    finish hw2_1

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -3,6 +3,51 @@
 #include "print_scene.h"
 #include "timer.h"
 
+
+bool RayIntersectsTriangle(ray localRay, 
+                           Triangle tri,
+                           Vector3& outIntersectionPoint,
+                           Vector3& baryC)
+{
+    const float EPSILON = 1e-7;
+
+    Vector3 h, s, q;
+    Real a, f, u, v;
+    h = cross(localRay.direction(), tri.e2);
+    a = dot(tri.e1, h);
+
+    if (a > -EPSILON && a < EPSILON) {
+        return false;    // This ray is parallel to this triangle.
+    }
+    f = 1.0 / a;
+    s = localRay.origin() - tri.p0;
+    u = f * dot(s, h);
+
+    if (u < 0.0 || u > 1.0) {
+        return false;   // hit point not in triangle
+    }
+
+    q = cross(s, tri.e1);
+    v = f * dot(localRay.direction(), q);
+
+    if (v < 0.0 || u + v > 1.0) {
+        return false;   // hit point not in triangle
+    }
+
+    // At this stage we can compute t to find out where the intersection point is on the line.
+    float t = f * dot(tri.e2, q);
+
+    if (t > EPSILON) // ray intersection
+    {
+        outIntersectionPoint = localRay.at(t);
+        baryC = {1.0-u-v, u, v};
+        return true;
+    }
+    else {// This means that there is a line intersection but not a ray intersection.
+        return false;
+    }
+}
+
 Image3 hw_2_1(const std::vector<std::string> &params) {
     // Homework 2.1: render a single triangle and outputs
     // its barycentric coordinates.
@@ -23,6 +68,7 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
             tri_params.push_back(std::stof(params[i]));
         }
     }
+    double inv_spp = 1.0 / spp;
 
     if (tri_params.size() < 9) {
         // Not enough parameters to parse the triangle vertices.
@@ -32,8 +78,44 @@ Image3 hw_2_1(const std::vector<std::string> &params) {
     Vector3 p0{tri_params[0], tri_params[1], tri_params[2]};
     Vector3 p1{tri_params[3], tri_params[4], tri_params[5]};
     Vector3 p2{tri_params[6], tri_params[7], tri_params[8]};
+    Triangle tri(p0, p1, p2);
 
     Image3 img(640 /* width */, 480 /* height */);
+
+    // Default camera described above; reuse hw1 code
+    Camera cam = {
+        Vector3(0, 0,  0),  // lookfrom;
+        Vector3(0, 0, -1),  // lookat
+        Vector3(0, 1,  0),  // up
+        45.0   // vfov
+    };
+
+    ray localRay;
+    Real u, v;
+    Vector3 pixel_pos;
+    // setup random geneator
+    pcg32_state rng = init_pcg32();
+    for (int y = 0; y < img.height; y++) {
+        for (int x = 0; x < img.width; x++) {
+            Vector3 pixel_color = {0.0, 0.0, 0.0};
+            for (int s=0; s<spp; ++s) { 
+                // shoot a random ray
+                u = Real(x + next_pcg32_real<double>(rng)) / (img.width - 1);
+                v = Real(y + next_pcg32_real<double>(rng)) / (img.height - 1);
+                localRay = cam.get_ray(u, v);
+
+                // NEW: ray triangle intersection
+                Vector3 hitPos, baryC;
+                if (RayIntersectsTriangle(localRay, tri, hitPos, baryC)) {
+                    pixel_color += Vector3(baryC);
+                } else{
+                    pixel_color += {0.5, 0.5, 0.5};
+                }
+            }
+            // write average color
+            img(x, img.height-1 - y) = pixel_color * inv_spp;
+        }
+    }
 
     return img;
 }
--- src/hw2.h
+++ src/hw2.h
@@ -1,9 +1,62 @@
 #pragma once
 
 #include "image.h"
+#include "ray.h"
+#include "pcg.h"
 
 Image3 hw_2_1(const std::vector<std::string> &params);
 Image3 hw_2_2(const std::vector<std::string> &params);
 Image3 hw_2_3(const std::vector<std::string> &params);
 Image3 hw_2_4(const std::vector<std::string> &params);
 Image3 hw_2_5(const std::vector<std::string> &params);
+
+// copied from hw1_scenes.h
+struct Camera
+{
+    Vector3 origin;
+    // Vector3 lookat;
+    // Vector3 up;
+    // Real vfov;
+    Vector3 horizontal, vertical;
+    Vector3 lower_left_corner;
+
+    Camera(
+        Vector3 lookfrom,
+        Vector3 lookat,
+        Vector3 vup,
+        double vfov, // vertical field-of-view in degrees
+        // give it a default value s.t. prev constructor still works.
+        double aspect_ratio = 4.0 / 3.0) : // origin(lookfrom), lookat(lookat), up(vup), vfov(vfov)
+                                            origin(lookfrom)
+    {
+        auto theta = radians(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2.0 * h;
+        auto viewport_width = aspect_ratio * viewport_height;
+
+        auto w = normalize(lookfrom - lookat); // opposite camera focus direction
+        auto u = normalize(cross(vup, w));     // "right"
+        auto v = cross(w, u);                  // projected "up"
+
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = lookfrom - horizontal / 2.0 - vertical / 2.0 - w;
+    }
+
+    ray get_ray(double offset_u, double offset_v) const
+    {
+        return ray(
+            origin,
+            lower_left_corner + offset_u * horizontal + offset_v * vertical - origin);
+    }
+};
+
+class Triangle {
+public:
+    Vector3 p0, p1, p2;  // vertices position
+    Vector3 e1, e2;  // p1-p0, p2-p0; not normalized
+
+    Triangle(Vector3 pos0, Vector3 pos1, Vector3 pos2) :
+        p0(pos0), p1(pos1), p2(pos2),
+        e1(pos1 - pos0), e2(pos2 - pos0) {}
+};
