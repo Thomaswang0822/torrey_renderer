3d4b7ff8a0130f072a28d4278f483742d179c626
thomaswang0822 <thomaswang0822@outlook.com>
2023-05-03 10:35:50 -0700

    map uv to color on texture img

--- src/material.h
+++ src/material.h
@@ -2,13 +2,34 @@
 
 #include "parse_scene.h"
 
+/**
+ * @brief One ImageTexture maps to one RGB img
+ * 
+ * @note: image data is written when the (only)
+ * constructor is called.
+ * 
+ */
 struct ImageTexture {
-    fs::path filename;
+    Image3 img3;
     Real uscale = 1, vscale = 1;
     Real uoffset = 0, voffset = 0;
 
     ImageTexture(const ParsedImageTexture& pImgTex) :
-        filename(pImgTex.filename) {}
+        img3(imread3(pImgTex.filename)) {}
+
+
+    /**
+     * @brief Given a uv position, return a Vector3 RGB on the texture image
+     * 
+     */
+    Vector3 color_value(double u, double v) {
+        // null guardian
+        if (img3.data.size() == 0) {
+            // TODO: change to scene background color
+            return Vector3(0.5, 0.5, 0.5);
+        }
+        
+    }
 };
 
 using Color = std::variant<Vector3 /* RGB */, ImageTexture>;
